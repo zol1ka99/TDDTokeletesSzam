cccc18f9b4ac5714e0d5c64d7b387e8ec365be0f
vsdeveloper10@outlook.com <vsdeveloper10@outlook.com>
2020-03-06 09:26:17 +0100

    anyad

---
+++ Rendezes/Rendezes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rendezes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int> { 28, 39, 33, 12, 9, 17 };
+            int n = a.Count;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                for (int j = n; j < a.Count; j++)
+                {
+                    if (true)
+                    {
+                        int temp = a.ElementAt(i);
+                        int a.ElementAt(i)
+                    }
+                }
+            }
+        }
+    }
+}
