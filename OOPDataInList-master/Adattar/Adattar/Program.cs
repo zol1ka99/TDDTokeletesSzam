80f94b21746acdca9b57f4d3fdb8d69e5f374f3a
Marton Zoltan <martonzoli99@gmail.com>
2020-03-06 10:26:29 +0100

    Push

--- OOPDataInList-master/Adattar/Adattar/Program.cs
+++ OOPDataInList-master/Adattar/Adattar/Program.cs
@@ -10,6 +10,34 @@ namespace ListakbanLevoAdatokKezeleseOOPalapokon
     {
         static void Main(string[] args)
         {
+            Persons p = new Persons();
+            List<Person> persons = p.getPersons();
+
+            int N = persons.Count;
+            //i-> ide kerül az aktuális legkisebb
+            for (int i = 0; i < N - 1; i = i + 1)
+            {
+                int min = persons[i].getAge();
+                int index = i;
+                // minimum keresés i..n-1
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (persons[j].getAge() < min)
+                    {
+                        min = persons[j].getAge();
+                        index = j;
+                    }
+                }
+                //csere
+                int temp = persons[i].getAge();
+                persons[i] = persons[index];
+                persons[index] = temp.Conve;
+            }
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(persons[i] + ",");
+            }
+            Console.ReadKey();
         }
     }
 }
